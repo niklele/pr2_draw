1f285541806633b62a40c1d64019f14d4e3778c1
Nikhil Lele <nik.lele@gmail.com>
2015-07-30 14:19:04 -0700

    remove drawing class, rework calculated orientations, linear interpolate position waypoints and slerp corresponding orientations

--- scripts/draw.py
+++ scripts/draw.py
@@ -2,140 +2,134 @@
 import roslib; roslib.load_manifest('pr2_draw')
 import rospy
 import tf
-# from tf.transformations import quaternion_from_euler as quaternion
 import numpy as np
 import numpy.linalg as la
 import draw_control as dc
 
-from scipy import interpolate
+def pt(x, y, z):
+    return np.array([x, y, z])
 
-def pt(x0,x1):
-    return np.array([x0, x1])
+def quat(x, y, z, w):
+    return np.array([x, y, z, w])
 
-class DrawingPlan(object):
-    """creates a trajectory to draw a shape"""
-    def __init__(self, draw_control):
-        super(DrawingPlan, self).__init__()
-        self.draw_control = draw_control
-        self.whiteboard = 0.8 # whiteboard defined as plane in at x-offset
+def calc_orientation(p0, p1, angle):
+    """
+    Calculate the gripper orientation required to stay at a certain angle to the segment from p0 to p1
+    p0 and p1 should be in /r_gripper_tool_frame
+    """
+    # method 1: roll wrist and then pitch to drawing angle
+    # roll = np.atan2((p1[1] - p0[1]) / (p1[0] - p0[0]))
+    # orientation = tf.transformations.quaternion_from_euler(roll, pitch, 0)
 
-    def send(self, path):
-        # path defined as sequence of (x,y) vertices on whiteboard
+    # method 2: construct quaternion to rotate arbitrarily about axis perpendicular to current segment
+    perp = np.cross([1,0,0], p1 - p0)
+    orientation = tf.transformations.quaternion_about_axis(angle, perp)
+    return orientation
 
-        # orientation = self.draw_control.home_orientation
-        sx = 100 # stiffness along x
-        angle = -np.pi / 6
+def calc_path_orientations(path):
+    """change path's orientations so that the marker stays at a certain angle to the drawing"""
+    # path is list of [pt, quat]
+    angle = -np.pi/6 # fixed angle
+    for i in xrange(len(path) - 1):
+        p0 = path[i][0]
+        p1 = path[i+1][0]
+        path[i][1] = calc_orientation(p0, p1, angle)
 
-        for i in xrange(len(path) - 1):
-            p0 = np.array([self.whiteboard, path[i][0], path[i][1]])
-            p1 = np.array([self.whiteboard, path[i+1][0], path[i+1][1]])
+    try:
+        path[-1][1] = path[-2][1] # use 2nd to last orientation for last orientation
+    except IndexError:
+        pass
+    return path
 
-            # method 1: roll wrist and then pitch to drawing angle
-            # roll = np.atan2((p1[1] - p0[1]) / (p1[0] - p0[0]))
-            # orientation = tf.transformations.quaternion_from_euler(roll, pitch, 0)
+def interpolate(path, max_dist):
+    """
+    Interpolate a path along position and orientation waypoints.
+    Ensures that each point is at most max_dist away from the next.
+    """
 
-            # method 2: construct quaternion to rotate arbitrarily about axis perpendicular to current path
-            perp = np.cross([1,0,0], p1 - p0)
-            orientation = tf.transformations.quaternion_about_axis(angle, perp)
+    # 1. linear interpolate positions with max_dist
+    # 2. slerp orientations to match interpolated positions
 
-            # actual = [self.whiteboard, p0[0], p0[1]]
-            rospy.loginfo("Draw point: {0} orientation: {1}".format(p0, orientation))
-            self.draw_control.move(p0, orientation, sx)
+    interp_path = []
+    for i in xrange(len(path) - 1):
+        p0 = path[i][0]
+        p1 = path[i+1][0]
 
-    def interpolate(self, path):
-        step = 0.01 # 1 cm
-        # interp_x = []
-        # interp_y = []
+        interp_pos = [] # (pos, fractional distance along segment)
 
-        interp_path = []
+        interp_pos.append((p0, 0.0)) # first point
 
-        for i in xrange(len(path) - 1):
-            p0 = path[i]
-            p1 = path[i+1]
-            interp_path.append(p0)
-            distance = la.norm(p1 - p0)
-            direction = (p1 - p0) / distance
-            new_p = np.array(p0, copy=True)
+        distance = la.norm(p1 - p0)
+        direction = (p1 - p0) / distance
+        new_p = np.array(p0, copy=True)
 
-            rospy.loginfo("interp step {0} of {1}".format(i+1, len(path)))
+        rospy.loginfo("interp step {0} of {1}".format(i+1, len(path)))
+        rospy.loginfo("position interpolation")
 
-            while True:
-                new_p += step * direction
+        while True:
+            new_p += step * direction
+            diff = la.norm(new_p - p0, 2)
+            if diff <= distance:
+                interp_pos.append((new_p, diff/distance))
+            else:
+                break
 
-                diff = la.norm(new_p - p0, 2)
+        interp_path.append((p1, 1.0)) # last point
 
-                # rospy.loginfo("p0:{0}, new_p:{1}, diff:{2}, dir:{3}".format(p0, new_p, diff, direction))
+        rospy.loginfo("orientation interpolation")
+        q0 = path[i][1]
+        q1 = path[i+1][1]
 
-                if diff <= distance:
-                    interp_path.append(new_p)
-                else:
-                    break
-            # interp_path.append(p1) # not necessary
+        for pos, fraction in interp_pos:
+            new_q = tf.transformations.quaternion_slerp(q0, q1, fraction)
+            interp_path.append([pos, new_q])
 
+    return interp_path
 
-        return interp_path
+def make_square(start_pos, length):
+    """create position waypoints to follow a square"""
+    path = []
+    path.append([start + pt(0, 0, 0), quat(0, 0, 0, 1)])
+    path.append([start + pt(0, 0, length), quat(0, 0, 0, 1)])
+    path.append([start + pt(0, length, 0), quat(0, 0, 0, 1)])
+    path.append([start + pt(0, length, length), quat(0, 0, 0, 1)])
+    path.append([start + pt(0, 0, length), quat(0, 0, 0, 1)])
+    path.append([start + pt(0, 0, 0), quat(0, 0, 0, 1)])
+    path.append([start + pt(0, 0, 0), quat(0, 0, 0, 1)])
+    return path
 
-
-    def square(self, length):
-        # build trajectory that draws a square
-        start_pos = []
-        while not start_pos:
-            start_pos = draw_control.curr_pos
-        rospy.loginfo("Drawing square starting at {0}".format(start_pos))
-
-        # use y,z as coordinates
-        start = pt(start_pos[1], start_pos[2])
-
-        path = []
-        path.append(start + pt(0,0))
-        path.append(start + pt(0,length))
-        path.append(start + pt(length,0))
-        path.append(start + pt(length,length))
-        path.append(start + pt(0,length))
-        path.append(start + pt(0,0))
-        path.append(start + pt(0,0))
-
-        path_interp = self.interpolate(path)
-
-        self.send(path_interp)
-
-    def line(self, length):
-
-        start_pos = []
-        while not start_pos:
-            start_pos = draw_control.curr_pos
-        rospy.loginfo("Drawing line starting at {0}".format(start_pos))
-
-        # use y,z as coordinates
-        start = pt(start_pos[1], start_pos[2])
-
-        path = []
-        path.append(start + pt(0, 0))
-        path.append(start + pt(length/2, 0))
-        path.append(start + pt(length, 0))
-
-        rospy.loginfo("path: {0}".format(path))
-
-        # path_interp = self.interpolate(path)
-        # self.send(path_interp)
-
-        self.send(path)
+def make_line(start_pos, length):
+    """create position waypoints to follow a line"""
+    path = []
+    path.append([start + pt(0, 0, 0), quat(0, 0, 0, 1)])
+    # path.append([start + pt(0, length/2, 0), quat(0, 0, 0, 1)])
+    path.append([start + pt(0, length, 0), quat(0, 0, 0, 1)])
+    return path
 
 if __name__ == '__main__':
     rospy.init_node('draw')
+    draw_control = dc.DrawController('right_arm')
 
-    draw_control = dc.DrawController('right_arm', True) # stiffness
-    drawing = DrawingPlan(draw_control)
+    start_offset = np.array([0.01, 0, 0]) # offset in /r_gripper_tool_frame
 
+    # 1. create position waypoints
+    # 2. create orientation waypoints to match positions
+    # 3. interpolate path
+
+    start_pos = []
+    while not start_pos:
+        start_pos = draw_control.curr_pos + start_offset
+    rospy.loginfo("Calculating line starting at {0}".format(start_pos))
+
+    line = make_line(start_pos, 0.2)
+    line_orientations = calc_path_orientations(line)
+    line_orientations_interp = interpolate(line_orientations, 0.01) # 1 cm max step
+
+    # Send the path
     draw_control.home()
-    drawing.square(0.1)
-
-    # drawing.line(0.2)
-
-    # draw_control.loiter(2) # seconds
-
+    draw_control.send_path(line_orientations_interp, 100)
     draw_control.home()
-
+    rospy.loginfo("Sending Goal Trajectory")
     draw_control.sendGoal()
 
     rospy.spin()
